ecb3fe28e229120c4a30dc1b54fc3058ccc4f757
Evan <evan.graine@gmail.com>
2020-05-28 23:00:35 +0200

    Fixed logger for retrocompatibility with logging library

--- core.py
+++ core.py
@@ -1,10 +1,13 @@
 # Libraries
 import time
+import json
 from datetime import datetime
 
 # Utilities
 from log import Logger
 
+
+logging = Logger() # TODO Change to Leo's logging script
 
 class Core():
     """ Orchestrator object. Can read configuration file to plan actions """
@@ -12,36 +15,48 @@ class Core():
     DATE_FORMAT = "%Y-%m-%d %H:%M"
     WAIT_INTERVAL = 1
 
-    def __init__(self, config="config.json"):
+    def __init__(self, initial_config="config.json"):
+        
         # Create a logger object.
-        self.logger = Logger(self)
-        self.logger.info("starting core")
+        logging.info("starting core")
+
+        if initial_config:
+            self.parse_config(initial_config)
 
     @staticmethod
     def parse_date(date_str: str):
         """ Parse a date from a string """
         date = datetime.strptime(date_str, format=Core.DATE_FORMAT)
-        self.logger.debug(f"parsing date {date_str} to datetime object {date}")
+        logging.debug(f"parsing date {date_str} to datetime object {date}")
         return date
         
 
     @staticmethod
     def date_to_str(date: datetime):
         date_str = date.strftime(Core.DATE_FORMAT)
-        self.logger.debug(f"putting date {date} to string {date_str}")
+        logging.debug(f"putting date {date} to string {date_str}")
         return date_str
 
     
     def wait_next_tick(self, interval=WAIT_INTERVAL):
         """ Waits for the next tick with the given interval between ticks """
         time_to_wait = interval - (time.time() % interval)
-        self.logger.debug(f"waiting {time_to_wait}ms before next tick")
+        logging.debug(f"waiting {time_to_wait}ms before next tick")
         time.sleep(time_to_wait)
 
 
+    def parse_config(self, config_path):
+        try:
+            config = json.load(open(config_path))
+        except FileNotFoundError as fnf_error:
+            logging.error(f"Config file {config_path} not found.")
+            return
+        
+        
+
     def mainloop(self):
         """ Starts the mainloop that processes idle tasks and waits the necessary time before the next tick """
-        self.logger.debug("starting main loop")
+        logging.debug("starting main loop")
         while True:
             # Wait for next tick
             self.wait_next_tick()
@@ -51,6 +66,6 @@ if __name__ == "__main__":
     # Tests
     config_path = "./test_config.json"
     
-    c = Core(config=config_path)
+    c = Core(initial_config=config_path)
     c.mainloop()
     
--- log.py
+++ log.py
@@ -7,8 +7,12 @@ LOG_LEVEL = "DEBUG"
 
 class Logger():
     """ Colored logging object based on *coloredlogs* library """
-    def __init__(self, object):
-        self.logger = logging.getLogger(object.__class__.__name__)
+    def __init__(self, object=None, name=""):
+        if object:
+            self.logger = logging.getLogger(object.__class__.__name__)
+        else:
+            self.logger = logging.getLogger(name)
+
         coloredlogs.install(level=LOG_LEVEL, logger=self.logger)
         self.debug = self.logger.debug
         self.info = self.logger.info
